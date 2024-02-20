f37c19ad9bb60e81d06712d01f59149041248249
root <root@DESKTOP-J0GRUU8>
2024-02-20 14:45:31 -0300

    Added Chrome cookie deleation

--- scripts/clearcookies.sh
+++ scripts/clearcookies.sh
@@ -6,7 +6,8 @@ echo "Removing cookies from last session..."
 for cookie in $(find "/home/aluno/.mozilla/firefox" -maxdepth 2 -name cookies.sqlite 2> /dev/null); do 
 
     rm -f ${cookie};
-    
+
 done
 
 # chrome
+rm -f /home/aluno/.config/google-chrome/Default/Cookies
