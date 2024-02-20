9ed858fd07dffacd5638751c0ad84d4db60a4722
root <root@DESKTOP-J0GRUU8>
2024-02-20 14:12:25 -0300

    Firefox cookies cleaning ok

--- scripts/clearcookies.sh
+++ scripts/clearcookies.sh
@@ -3,8 +3,10 @@
 # firefox
 echo "Removing cookies from last session..."
 
-for cookie in $(find "/home/aluno/.mozilla/firefox" -maxdepth 1 -name cookies.sqlite 2> /dev/null); do 
+for cookie in $(find "/home/aluno/.mozilla/firefox" -maxdepth 2 -name cookies.sqlite 2> /dev/null); do 
+
     rm -f ${cookie};
+    
 done
 
 # chrome
